d76c8401cde4676187a59e7e0ff8353d2272a9b3
WildanFathir <wildanfathir612@gmail.com>
2024-10-12 23:38:43 +0700

    Add Instagram, Navigation, and Whatsapp icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as Instagram } from '@/components/icons/Instagram.vue'
+export { default as Whatsapp } from '@/components/icons/Whatsapp.vue'
+export { default as Navigasi } from '@/components/icons/Navigation.vue'
