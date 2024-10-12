7851c68316c62d4ce33282f4b6634e27a42c9a33
WildanFathir <wildanfathir612@gmail.com>
2024-10-12 23:38:57 +0700

    feat: add Footer component with social media icons and typed text
    Add a new Footer component to the project that includes social media icons for Instagram, Navigation, and Whatsapp. The Footer also includes a typed text effect using the Typed.js library. This component enhances the overall user experience and provides easy access to social media platforms.
    
    ---
    
    refactor: update ProductPage layout and content
    
    Update the layout and content of the ProductPage component. This includes adding a background image, displaying product information with images and descriptions, and applying animations using the AOS library. These changes improve the visual appeal and presentation of the product information on the page.

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer
+    class="flex flex-col space-y-5 justify-center items-center bg-[#A27F6B] py-16"
+  >
+    <p class="text-lg font-medium text-white">
+      <span ref="typedElement"></span>
+    </p>
+
+    <div class="flex items-center justify-center space-x-6">
+      <a href="#">
+        <Whatsapp />
+      </a>
+      <a href="#">
+        <Instagram />
+      </a>
+      <a href="#">
+        <Navigasi />
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { Instagram, Navigasi, Whatsapp } from '@/components/icons'
+import { ref, onMounted } from 'vue'
+
+import Typed from 'typed.js'
+
+const typedElement = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  const options = {
+    strings: ['where to find us?'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    showCursor: true,
+    loop: true,
+  }
+
+  if (typedElement.value) {
+    new Typed(typedElement.value, options)
+  }
+})
+</script>
--- src/views/pages/ProductPage.vue
+++ src/views/pages/ProductPage.vue
@@ -1,3 +1,95 @@
 <template>
-  <h1>Hello Wolrd</h1>
+  <div>
+    <section
+      class="bg-[url('@/assets/images/bg-product.png')] bg-cover bg-center h-[373px] z-0"
+    ></section>
+
+    <div class="bg-[#D4BB9D]">
+      <div class="flex flex-col pt-20 pb-16 mx-8 space-y-20">
+        <h1 class="text-5xl italic font-bold">Our Product</h1>
+
+        <div class="flex items-center justify-between space-x-12">
+          <img
+            data-aos="zoom-in-right"
+            src="@/assets/images/mie-pangsit.png"
+            alt="pangsit"
+          />
+          <div
+            data-aos="zoom-in-left"
+            class="flex flex-col space-y-2 text-left"
+          >
+            <h1 class="text-3xl italic font-bold">MIE PANGSIT</h1>
+            <p
+              class="text-2xl text-justify"
+              style="font-family: 'Kotta One', serif"
+            >
+              Mie Pangsit merupakan hidangan mie dengan mie wangi daun seledri
+              ditambah toping tahu dan pangsit full isi ayam. Dengan rasa
+              campuran antara saus cabe, saus tomat dan kecap sehingga cocok
+              untuk menemani dikala lapar dan dijadikan menu kenyang dalam versi
+              yang pas
+            </p>
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between space-x-12">
+          <img
+            data-aos="zoom-in-right"
+            src="@/assets/images/pangsit-ayam.png"
+            alt="pangsit"
+          />
+          <div
+            data-aos="zoom-in-left"
+            class="flex flex-col space-y-2 text-left"
+          >
+            <h1 class="text-3xl italic font-bold">
+              PANGSIT AYAM KUAH CHILI OIL
+            </h1>
+            <p
+              class="text-2xl text-justify"
+              style="font-family: 'Kotta One', serif"
+            >
+              Pangsit Ayam Kuah Chili Oil merupakan menu makanan favorit para
+              pecinta pedas. Dibuat dengan rasa kuah yang khas, setiap suapan
+              adalah perjalanan rasa yang menggugah selera. Kenikmatan pedasnya
+              akan membuat lidah Anda ingin menari-nari
+            </p>
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between space-x-12">
+          <img
+            data-aos="zoom-in-right"
+            src="@/assets/images/dimsum.png"
+            alt="pangsit"
+          />
+          <div
+            data-aos="zoom-in-left"
+            class="flex flex-col space-y-2 text-left"
+          >
+            <h1 class="text-3xl italic font-bold">DIMSUM</h1>
+            <p
+              class="text-2xl text-justify"
+              style="font-family: 'Kotta One', serif"
+            >
+              Dimsum Ayam lezat berkualitas tinggi dibuat dengan cinta yang
+              dikukus hingga sempurna, menghasilkan tekstur yang kenyal diluar
+              dan lembut didalam. Dengan rasa indah dan tambahan yang menggoda
+              diseriap gigitannya dijamin akan membuat mood Anda kembali stabil
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import AOS from 'aos'
+
+onMounted(() => {
+  AOS.init()
+})
+</script>
