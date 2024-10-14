45f217a8e1b6b97533d7e3d9e0d4e7f202605905
WildanFathir <wildanfathir612@gmail.com>
2024-10-14 20:26:29 +0700

    Refactor: Update ContactPage.vue with responsive layout and typing animation

--- src/views/pages/ContactPage.vue
+++ src/views/pages/ContactPage.vue
@@ -1,3 +1,87 @@
 <template>
-  <h1>Hello World</h1>
+  <div
+    class="bg-gradient-to-b from-[#f2d1b3] via-[#87b4b0] to-[#f2d1b3] p-8 md:p-20 flex flex-col items-center justify-center min-h-screen w-full"
+  >
+    <div class="mb-4 text-center">
+      <div class="mb-6 text-2xl font-bold text-center md:text-4xl">
+        <span ref="typedElement"></span>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-2xl p-6 bg-transparent rounded-lg shadow-lg md:max-w-4xl"
+    >
+      <form class="space-y-5">
+        <div class="mb-4">
+          <label for="name" class="block text-sm font-medium">Name</label>
+          <input
+            type="text"
+            id="name"
+            class="block w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+            placeholder="Masukan nama anda"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label for="email" class="block text-sm font-medium">Email</label>
+          <input
+            type="email"
+            id="email"
+            class="block w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+            placeholder="Masukan email anda"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label for="phone" class="block text-sm font-medium"
+            >No. Telepon</label
+          >
+          <input
+            type="tel"
+            id="phone"
+            class="block w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+            placeholder="Masukan nomor telepon anda"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label for="message" class="block text-sm font-medium">Message</label>
+          <textarea
+            id="message"
+            rows="4"
+            class="block w-full p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-teal-500 focus:border-teal-500"
+            placeholder="Masukan pesan anda"
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          class="w-full px-4 py-2 text-white rounded-md shadow-md bg-slate-500 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+import Typed from 'typed.js'
+
+const typedElement = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  const options = {
+    strings: ['Any question or remarks?', 'Just write us a message!'],
+    typeSpeed: 60,
+    backSpeed: 20,
+    backDelay: 3000,
+    loop: true,
+  }
+
+  if (typedElement.value) {
+    new Typed(typedElement.value, options)
+  }
+})
+</script>
