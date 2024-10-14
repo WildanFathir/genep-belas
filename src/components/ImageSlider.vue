5a55ab9cb11d87a26ada445e4eb78bd55e01f952
WildanFathir <wildanfathir612@gmail.com>
2024-10-14 15:56:58 +0700

    feat: Add ImageSlider component for testimonial page

---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="bottom-0 flex items-center w-full p-8 overflow-hidden bg-primary-500"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <div class="flex animation slider shrink-0" :class="{ paused: isHovered }">
+      <div
+        class="flex gap-[50px] items-center"
+        style="width: 200%; animation: slide 30s linear infinite"
+      >
+        <div v-for="n in 2" :key="n" class="flex gap-[50px]">
+          <TestimonialCard
+            v-for="(testimonial, index) in userTesti"
+            :key="index"
+            :user="testimonial"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { userTesti } from '@/data/testimonials'
+import TestimonialCard from '@/components/TestimonialCard.vue'
+
+const isHovered = ref(false)
+</script>
+
+<style scoped>
+.animation {
+  animation: slide 30s linear infinite;
+}
+
+.paused {
+  animation-play-state: paused; /* Menambahkan efek berhenti saat hover */
+}
+
+@keyframes slide {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
+</style>
--- src/views/pages/TestimonialPage.vue
+++ src/views/pages/TestimonialPage.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Hello World</h1>
+  <div
+    class="flex justify-center items-center flex-col space-y-10 py-10 bg-[#D4BB9D]"
+  >
+    <div class="text-5xl italic font-bold">
+      <span ref="typedElement"></span>
+    </div>
+    <ImageSlider />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+import ImageSlider from '@/components/ImageSlider.vue'
+import Typed from 'typed.js'
+
+const typedElement = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  const options = {
+    strings: ['Testimonial'],
+    typeSpeed: 100,
+  }
+
+  if (typedElement.value) {
+    new Typed(typedElement.value, options)
+  }
+})
+</script>
